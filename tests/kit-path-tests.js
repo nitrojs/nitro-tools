7676b50e96758d7ccb1b28c658f39a655f38f2b0
Jesús Germade <jesus@germade.es>
2016-08-02 00:51:18 +0200

    fixed joinPath

--- tests/kit-path-tests.js
+++ tests/kit-path-tests.js
@@ -32,4 +32,24 @@ describe('joinPath', function () {
 		assert.equal( _.joinPath('api', 'v1', 'foo', 'bar'), 'api/v1/foo/bar');
 	});
 
+	it('_.joinPath 4 args slashes', function () {
+		assert.equal( _.joinPath('/api/', '/v1/', '/foo/', '/bar'), '/api/v1/foo/bar');
+	});
+
+	it('_.joinPath 4 args slashes rest', function () {
+		assert.equal( _.joinPath('/api/:apiId/', '/v1/:v1Id/', '/foo/:fooId/', '/bar/:barId'), '/api/:apiId/v1/:v1Id/foo/:fooId/bar/:barId');
+	});
+
+	it('_.joinPath 4 args slashes-alt rest', function () {
+		assert.equal( _.joinPath('/api/:apiId/', 'v1/:v1Id/', '/foo/:fooId/', 'bar/:barId'), '/api/:apiId/v1/:v1Id/foo/:fooId/bar/:barId');
+	});
+
+	it('_.joinPath 4 args slashes-alt2 rest', function () {
+		assert.equal( _.joinPath('/api/:apiId', '/v1/:v1Id', '/foo/:fooId', '/bar/:barId'), '/api/:apiId/v1/:v1Id/foo/:fooId/bar/:barId');
+	});
+
+	it('_.joinPath 4 args slashes-alt2 rest back', function () {
+		assert.equal( _.joinPath('/api/:apiId', '/v1/:v1Id', '../../foo/:fooId', '/bar/:barId'), '/api/:apiId/foo/:fooId/bar/:barId');
+	});
+
 });
